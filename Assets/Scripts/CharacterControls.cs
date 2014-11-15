7795214a723e343ea9f15106e52955f181fedce2
Andre Shuvalov <andre@2000-206F.local>
2014-11-15 15:09:38 -0500

    mouselook in charactercontrolls

--- Assets/Scripts/CharacterControls.cs
+++ Assets/Scripts/CharacterControls.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 [RequireComponent (typeof (Rigidbody))]
 [RequireComponent (typeof (CapsuleCollider))]
+[AddComponentMenu("Camera-Control/Mouse Look")]
 
 public class CharacterControls : MonoBehaviour {
 	
@@ -12,14 +13,49 @@ public class CharacterControls : MonoBehaviour {
 	public bool canJump = true;
 	public float jumpHeight = 2.0f;
 	private bool grounded = false;
+
+	public enum RotationAxes { MouseXAndY = 0, MouseX = 1, MouseY = 2 }
+	public RotationAxes axes = RotationAxes.MouseXAndY;
+	public float sensitivityX = 15F;
+	public float sensitivityY = 15F;
 	
+	public float minimumX = -360F;
+	public float maximumX = 360F;
 	
+	public float minimumY = -60F;
+	public float maximumY = 60F;
+	
+	float rotationY = 0F;
 	
 	void Awake () {
 		rigidbody.freezeRotation = true;
 		rigidbody.useGravity = false;
 	}
-	
+
+	void Update ()
+	{
+		if (axes == RotationAxes.MouseXAndY)
+		{
+			float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+			
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+			
+			transform.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+		}
+		else if (axes == RotationAxes.MouseX)
+		{
+			transform.Rotate(0, Input.GetAxis("Mouse X") * sensitivityX, 0);
+		}
+		else
+		{
+			rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+			rotationY = Mathf.Clamp (rotationY, minimumY, maximumY);
+			
+			transform.localEulerAngles = new Vector3(-rotationY, transform.localEulerAngles.y, 0);
+		}
+	}
+
 	void FixedUpdate () {
 		if (grounded) {
 			// Calculate how fast we should be moving
@@ -51,6 +87,7 @@ public class CharacterControls : MonoBehaviour {
 		grounded = true;    
 	}
 	
+
 	float CalculateJumpVerticalSpeed () {
 		// From the jump height and gravity we deduce the upwards speed 
 		// for the character to reach at the apex.
