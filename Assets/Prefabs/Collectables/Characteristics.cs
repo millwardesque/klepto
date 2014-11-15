c9bb57c21c0a9ec881fce43467751760d537838c
Christopher Millward <christopher.millward@gmail.com>
2014-11-15 12:33:30 -0500

    Added Prefabs and Scripts.

---
+++ Assets/Prefabs/Collectables/Characteristics.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Characteristics : MonoBehaviour {
+
+	public float weight = 1.0f;
+	public float size = 1.0f;
+	public float value = 1.0f;
+	public float breakability = 0.0f;
+
+	public float Weight {
+		get {
+			return weight;
+		}
+
+		set {
+			weight = value;
+		}
+	}
+
+	public float Size {
+		get {
+			return size;
+		}
+
+		set {
+			size = value;
+		}
+	}
+
+	public float Value {
+		get {
+			return value;
+		}
+
+		set {
+			this.value = value;
+		}
+	}
+
+	public float Breakabilaty {
+		get {
+			return breakability;
+		}
+
+		set {
+			breakability = value;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/TestMove.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestMove : MonoBehaviour {
+	private CharacterMotor characterMotor;
+
+	// Use this for initialization
+	void Start () {
+		characterMotor = GetComponent<CharacterMotor>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		characterMotor.movement.maxForwardSpeed = 1.0f;
+	}
+}
